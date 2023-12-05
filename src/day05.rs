982138416d4a8282ca9a2cc285351e728d14cafb
Charlie Wilson <charles.ep.wilson@gmail.com>
2023-12-05 23:14:37 +0000

    Day 5 part 2

--- src/day05.rs
+++ src/day05.rs
@@ -2,17 +2,48 @@ use std::slice::Iter;
 use crate::utils::read_lines;
 
 pub fn part1() -> usize {
-    let (seeds, maps) = parse_input();
-    let locations: Vec<usize> = seeds.iter().map(|&x| find_location(x, &maps)).collect();
-    *locations.iter().min().unwrap()
+    let (seeds, layers) = parse_input();
+    find_min_location(seeds, layers)
 }
 pub fn part2() -> usize {
-    let r = 0;
-    r
+    let (seeds, layers) = parse_input();
+    let seed_pairs: Vec<(usize, usize)> = seeds.chunks_exact(2).map(|x| (x[0], x[1])).collect();
+    let mut bands = Vec::new();
+    for (start, range) in seed_pairs {
+        bands.push((start, start + range - 1));
+    }
+    for layer in layers {
+        let mut new_bands = Vec::new();
+        for (band_min, band_max) in bands.iter() {
+            new_bands.extend(layer.split_band(*band_min, *band_max));
+        }
+        let mut traversed_bands = Vec::new();
+        for (band_min, band_max) in new_bands.iter() {
+            traversed_bands.push(layer.traverse_band(*band_min, *band_max));
+        }
+        bands = traversed_bands
+    }
+    let mut locations = Vec::new();
+    for (b, t) in bands {
+        locations.push(b);
+        locations.push(t);
+    }
+    *locations.iter().min().unwrap()
+}
+
+fn find_min_location<I>(seeds: I, layers: Vec<Layer>) -> usize
+    where
+        I: IntoIterator<Item = usize>,
+{
+    let mut locations = Vec::new();
+    for seed in seeds {
+        locations.push(find_location(seed, &layers));
+    }
+    *locations.iter().min().unwrap()
 }
 
 
-fn parse_input() -> (Vec<usize>, Vec<Vec<(usize, usize, usize)>>) {
+fn parse_input() -> (Vec<usize>, Vec<Layer>) {
     let mut lines = Vec::new();
     if let Ok(buf_lines) = read_lines("./input/day05.txt") {
         for line in buf_lines {
@@ -32,7 +63,7 @@ fn parse_input() -> (Vec<usize>, Vec<Vec<(usize, usize, usize)>>) {
         let map = parse_section(iter);
         maps.push(map);
     }
-    (seeds, maps)
+    (seeds, layers_from_maps(maps))
 }
 
 fn parse_seeds(seeds_str: &String) -> Vec<usize> {
@@ -48,15 +79,83 @@ fn parse_section(lines: Iter<String>) -> Vec<(usize, usize, usize)> {
     maps
 }
 
-fn find_location(seed: usize, maps: &Vec<Vec<(usize, usize, usize)>>) -> usize {
-    let mut value = seed;
+fn layers_from_maps(maps: Vec<Vec<(usize, usize, usize)>>) -> Vec<Layer> {
+    let mut layers = Vec::new();
     for map in maps {
-        for (min_dest, min_source, range) in map {
-            if (*min_source <= value) & (value < min_source + range) {
-                value = (value + min_dest) - min_source;
-                break;
-            }
-        }
+        layers.push(Layer::from_ranges(map));
+    }
+    layers
+}
+
+fn find_location(seed: usize, layers: &Vec<Layer>) -> usize {
+    let mut value = seed;
+    for layer in layers {
+        value = layer.traverse(value);
     }
     value
+}
+
+fn in_band(value: usize, band_min: usize, band_max: usize) -> bool {
+    (value >= band_min) & (value <= band_max)
+}
+
+struct Layer {
+    pipes: Vec<(usize, usize, isize)>,
+}
+
+impl Layer {
+    fn from_ranges(mut ranges: Vec<(usize, usize, usize)>) -> Self {
+        let mut pipes = Vec::new();
+        // rethink each map as a series of "pipes"
+        // each of which adds a modifier to values that fall within it's range
+        ranges.sort_by_key(|(_, s, _)| *s);
+        let mut last_pipe_end: usize = 0;
+        for (min_dest, min_source, range) in  ranges {
+            let pipe_start = min_source;
+            let pipe_end = pipe_start + range - 1;
+            let modifier = (min_dest as isize) - (min_source as isize);
+            let dead_pipe_start = last_pipe_end + 1;
+            // Add "dead pipe" to explicitly handle the case where the value isn't specified
+            if (dead_pipe_start != pipe_start)  & (dead_pipe_start != 1) {
+                pipes.push((dead_pipe_start, pipe_start - 1, 0));
+            }
+            pipes.push((pipe_start, pipe_end, modifier));
+            last_pipe_end = pipe_end;
+        }
+        Self{pipes}
+    }
+
+    fn traverse(&self, value: usize) -> usize {
+        for (pipe_start, pipe_end, modifier) in &self.pipes {
+            if in_band(value, *pipe_start, *pipe_end) {
+                return ((value as isize) + modifier) as usize;
+            }
+        }
+        value
+    }
+
+    fn split_band(&self, band_min: usize, band_max: usize) -> Vec<(usize, usize)> {
+        let mut new_bands: Vec<(usize, usize)> = Vec::new();
+        for (start, end, _) in self.pipes.iter().cloned() {
+            let start_in = in_band(start, band_min, band_max);
+            let end_in = in_band(end, band_min, band_max);
+            if (start <= band_min) & end_in {
+                new_bands.push((band_min, end))
+            }
+            if start_in & end_in {
+                new_bands.push((start, end));
+            }
+            if start_in & (end >= band_max) {
+                new_bands.push((start, band_max));
+            }
+            if (start <= band_min) & (end >= band_max) {
+                new_bands.push((band_min, band_max));
+            }
+        }
+        new_bands
+    }
+
+    fn traverse_band(&self, band_min: usize, band_max: usize) -> (usize, usize) {
+        (self.traverse(band_min), self.traverse(band_max))
+    }
 }
