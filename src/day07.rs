19560dc25eb44bf8ca40409bdb61895eaf19fa05
Charles Wilson <charles.wilson@tradenation.com>
2023-12-07 14:31:03 +0000

    Day 7 part 2

--- src/day07.rs
+++ src/day07.rs
@@ -8,13 +8,16 @@ pub fn part1() -> usize {
     winnings.sum()
 }
 pub fn part2() -> usize {
-    // let input = parse_input();
-    0
+    let mut input = parse_input();
+    input.sort_by_cached_key(joker_sort_key);
+    let winnings = input.iter().enumerate().map(|(rank, (_, bid))| bid * (rank + 1));
+    winnings.sum()
 }
 
 #[derive(Copy, Clone, Debug, PartialOrd, PartialEq, Ord, Eq, Hash)]
 enum Card {
-    Two = 2,
+    Joker = 1,
+    Two,
     Three,
     Four,
     Five,
@@ -53,6 +56,7 @@ impl From<char> for Card {
 
 #[derive(Copy, Clone, Debug, PartialOrd, PartialEq, Ord, Eq)]
 enum HandType {
+    Empty,
     HighCard,
     Pair,
     TwoPair,
@@ -61,6 +65,21 @@ enum HandType {
     FourOfKind,
     FiveOfKind,
 }
+
+impl HandType {
+    fn upgrade(self) -> Self {
+        match self {
+            Self::Empty => Self::HighCard,
+            Self::HighCard => Self::Pair,
+            Self::Pair => Self::ThreeOfKind,
+            Self::TwoPair => Self::FullHouse,
+            Self::ThreeOfKind => Self::FourOfKind,
+            _ => Self::FiveOfKind,
+        }
+    }
+}
+
+
 type Hand = Vec<Card>;
 
 
@@ -79,27 +98,32 @@ fn parse_input() -> Vec<(Hand, usize)> {
 }
 
 fn categorise_hand(hand: &Hand) -> HandType {
+    let (distinct, count) = get_distinct_and_count(hand);
+    categorise_from_distinct_count(distinct, count)
+}
+fn get_distinct_and_count(hand: &Hand) -> (usize, usize) {
     let card_set: HashSet<_> = hand.iter().map(|x| *x).collect();
     let distinct = card_set.len();
-    match distinct {
-        5 => return HandType::HighCard,
-        4 => return HandType::Pair,
-        1 => return HandType::FiveOfKind,
-        _ => (),
-    }
     let mut counts = Vec::new();
     for card in hand {
         counts.push(hand.iter().filter(|&x| x == card).count());
     }
-    let max_count = counts.iter().max().unwrap();
-    match (distinct, max_count) {
-        (_, 2) => HandType::TwoPair,
-        (_, 4) => HandType::FourOfKind,
-        (3, 3) => HandType::ThreeOfKind,
-        (2, 3) => HandType::FullHouse,
-        _ => panic!(),
-    }
+    let max_count = counts.iter().max().unwrap_or(&0);
+    (distinct, *max_count)
+}
 
+fn categorise_from_distinct_count(distinct: usize, count: usize) -> HandType {
+    match (distinct, count) {
+        (_, 0) => HandType::Empty,
+        (_, 1) => HandType::HighCard,
+        (4, 2) => HandType::Pair,
+        (_, 2) => HandType::TwoPair,
+        (2, 3) => HandType::FullHouse,
+        (3, 3) => HandType::ThreeOfKind,
+        (_, 4) => HandType::FourOfKind,
+        (_, 5) => HandType::FiveOfKind,
+        _ => panic!("{} {}", distinct, count),
+    }
 }
 
 fn sort_key(h: &(Hand, usize)) -> (HandType, Hand) {
@@ -107,4 +131,30 @@ fn sort_key(h: &(Hand, usize)) -> (HandType, Hand) {
     (categorise_hand(hand), hand.iter().map(|x| *x).collect())
     // This is bad...I couldn't work out the lifetime problem that stopped me from just returning
     // `(categorise_hand(hand), hand)` and this is the easiest way I could think of to clone the hand vector :/
+}
+
+fn joker_sort_key(h: &(Hand, usize)) -> (HandType, Hand) {
+    let (hand, _) = h;
+    let mut jokerless: Vec<Card> = Vec::new();
+    let mut jokerful: Vec<Card> = Vec::new();
+    let mut num_jokers: usize = 0;
+    for card in hand {
+        match card {
+            Card::Jack => {
+                num_jokers += 1;
+                jokerful.push(Card::Joker);
+            }
+            _ => {
+                jokerless.push(*card);
+                jokerful.push(*card);
+            }
+        }
+    }
+    let (sub_distinct, count) = get_distinct_and_count(&jokerless);
+    let distinct = sub_distinct + (5 - jokerless.len());
+    let mut hand_type = categorise_from_distinct_count(distinct, count);
+    for _ in 0..num_jokers {
+        hand_type = hand_type.upgrade();
+    }
+    (hand_type, jokerful)
 }
